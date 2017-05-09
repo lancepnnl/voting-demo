f71ccac9fc1ad5447b9299d5a55a1514de577b0a
PNL\D3P241 <lance.irvine@pnnl.gov>
2017-05-09 15:40:25 -0700

    changing the voting options

--- voting/app.py
+++ voting/app.py
@@ -9,8 +9,8 @@ import logging
 import signal
 import sys
 
-optionA = "NFC"
-optionB = "AFC"
+optionA = "ABA"
+optionB = "NBA"
 name = optionA + " VS " + optionB
 
 redis = Redis(host="redis", db=0)
